29cf0745f82387da645f5722c935de8e6d6d2542
Aditya Pansari <iadityapansari@gmail.com>
2022-05-13 18:58:19 +0530

    setup webpack for marketing app

---
+++ marketing/config/webpack.common.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+            plugins: [
+              ["@babel/plugin-transform-runtime", { regenerator: true }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
