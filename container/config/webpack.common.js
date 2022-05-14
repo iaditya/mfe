60e19020af7e49902f18b7e1f6561602c0cb4d2e
Aditya Pansari <iadityapansari@gmail.com>
2022-05-14 11:07:25 +0530

    container: adds prod webpack config

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -19,4 +21,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
