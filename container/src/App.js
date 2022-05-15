c4ff45068a0bba875cb04b9cecbb857ae0f062c5
Aditya Pansari <iadityapansari@gmail.com>
2022-05-15 23:08:54 +0530

    fix css [conflicting on prod when minifies] -> provided prefix name to avoid.

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Header from "./components/Header";
 import MarketingApp from "./remoteComponents/MarketingApp";
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-  </BrowserRouter>
-);
+const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
+
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+  });
+
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing}></Route>
