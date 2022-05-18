9823e1cc2e5bd1c896f53c4a8fdef1577d3a4b7c
Aditya Pansari <iadityapansari@gmail.com>
2022-05-18 17:33:18 +0530

    minor

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const commonConfig = require("./webpack.common");
